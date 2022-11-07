9fd2eae722abc59a3f81e1588aed1e118147bc11
Thaddybear <thaddeuschong262@gmail.com>
2022-11-07 20:20:06 +0800

    Update Seat.java

--- src/entities/Seat.java
+++ src/entities/Seat.java
@@ -1,37 +1,48 @@
 package entities;
 
 public class Seat {
+    public enum Type {Normal, Couple, Elite, Ultima};
+
+    private Type type;
     private int seatID;
     private boolean available;
     private int ticketHolder;
 
-    public Seat(int seatID, boolean available, int ticketholder){
+    public Seat(Type type, int seatID, boolean available, int ticketholder){
+        this.type = type;
         this.seatID = seatID;
         this.available = available;
         this.ticketHolder = ticketholder;
     }
 
-    public boolean getAvailability(){
+    //getters
+    public boolean isAvailable() {
         return this.available;
     }
-
     public int getSeatID(){
         return this.seatID;
     }
-
-    public void setSeatID(int seatID) {
-        this.seatID = seatID;
-    }
-
     public int getTicketholder(){
         return this.ticketHolder;
     }
+    public Type getType() {
+        return this.type;
+    }
 
+    //setters
+    public void setSeatID(int seatID) {
+        this.seatID = seatID;
+    }
     public void setAvailability(boolean newAvailability){
         this.available = newAvailability;
     }
-
     public void setTicketHolder(int ticketHolder){
         this.ticketHolder = ticketHolder;
+    }
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+    public void setType(Type type) {
+        this.type = type;
     }
 }
