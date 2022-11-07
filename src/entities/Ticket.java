f2c4cc6081cc5e8ab3e146d13d3fe79d426a9596
Tirth3033 <97597183+Tirth3033@users.noreply.github.com>
2022-11-07 20:57:12 +0800

    New commit

---
+++ src/entities/Ticket.java
@@ -0,0 +1,126 @@
+package entities;
+
+public class Ticket
+{
+	public enum TicType {
+		SENIOR, ADULT, CHILD
+	};
+
+
+	private int userID;
+	private int ticketID;
+	private TicType ticketType;
+	private String movieTime;
+	private int movieID;
+	private String movieName;
+	private double price;
+	private String cinemaName;
+	private int seatID;
+
+	public Ticket(int userID, String movieName)
+	{
+		this.userID= userID;
+		this.price=0.0;
+		this.movieName=movieName;
+	}
+	
+	//get methods
+	public int getUserID()
+	{
+		return userID;
+	}
+
+	public int getTicketID()
+	{
+		return ticketID;
+	}
+	
+	public TicType getTicketType()
+	{
+		return ticketType;
+	}
+	
+	public String getMovieTime()
+	{
+		return movieTime;
+	}
+	
+	public int getMovieID()
+	{
+		return movieID;
+	}
+	
+	public String getMovieName()
+	{
+		return movieName;
+	}
+	
+	public double getPrice()
+	{
+		return price;
+	}
+	
+	public String getCinemaName()
+	{
+		return cinemaName;
+	}
+	
+	public int getSeatID()
+	{
+		return seatID;
+	}
+
+
+
+	//setter methods
+	public void setUserID(int userID)
+	{
+		this.userID=userID;
+	}
+
+	public void setTicketID(int ticketID)
+	{
+		this.ticketID=ticketID;
+	}
+	
+	public void setTicketType(TicType ticketType)
+	{
+		this.ticketType=ticketType;
+	}
+	
+	public void setMovieTime(String movieTime)
+	{
+		this.movieTime=movieTime;
+	}
+	
+	public void setMovieID(int movieID)
+	{
+		this.movieID=movieID;
+	}
+	
+	public void setMovieName(String MovieName)
+	{
+		this.movieName=movieName;
+	}
+	
+	public void setPrice(double price)
+	{
+		this.price=price;
+	}
+	
+	public void setCinemaName(String cinemaName)
+	{
+		this.cinemaName=cinemaName;
+	}
+	
+	public void setSeatID(int seatID)
+	{
+		this.seatID=seatID;
+	}
+	
+	public void printTicketInfo()
+	{
+		System.out.println("User ID: "+userID+"\nTicket ID: "+ticketID+"\nTicket Type: "+ticketType+"\nMovie Time: "+ movieTime + "\nMovie ID: "+ movieID +"\nMovie Name: "+ movieName +"\nPrice: " + price +"\nCinema Name: "+ cinemaName +"\nSeat ID: "+seatID);
+	}
+	
+}
