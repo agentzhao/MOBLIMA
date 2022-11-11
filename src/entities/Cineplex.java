93ac3c243c2a016e203c6de355d808aa2e75ccdd
Thaddybear <thaddeuschong262@gmail.com>
2022-11-11 15:53:17 +0800

    update

--- src/entities/Cineplex.java
+++ src/entities/Cineplex.java
@@ -1,19 +1,34 @@
 package entities;
 
 public class Cineplex{
+    private String cineplexName;
     private String cineplexID;
     private Cinema[] cinemas;
 
-    public Cineplex(String cineplexID, Cinema[] cinemas){
+    public Cineplex(String cineplexName, String cineplexID, Cinema[] cinemas){
         this.cineplexID = cineplexID;
         this.cinemas = cinemas;
     }
-
+    
+    //Getters
     public String getCineplexID() {
         return cineplexID;
     }
-
     public Cinema[] getCinemas() {
         return cinemas;
+    }
+    public String getCineplexName() {
+        return cineplexName;
+    }
+
+    //Setters
+    public void setCineplexName(String cineplexName) {
+        this.cineplexName = cineplexName;
+    }
+    public void setCineplexID(String cineplexID) {
+        this.cineplexID = cineplexID;
+    }
+    public void setCinemas(Cinema[] cinemas) {
+        this.cinemas = cinemas;
     }
 }
