b155b19e2265ae60c2421cdc98758d87f344e91f
Keith Sim <keithsimks@gmail.com>
2022-11-11 15:56:55 +0800

    added comments

--- src/utils/MoblimaInitializer.java
+++ src/utils/MoblimaInitializer.java
@@ -23,23 +23,35 @@ public class MoblimaInitializer {
   private String dataPath;
   
   
+  /*
+   * MoblimaInitializer Constructor
+   * dataPath - Path containing all initialisation .txt files.
+   */
   public MoblimaInitializer() {
     dataPath = Paths.get("").toAbsolutePath() + "/data/initialization_files";
   }
   
   
+  /*
+   * Initialise movie data
+   * Returns: List of movie objects
+   */
   public List<Movie> initializeMovie() {
+    /* Get list of Movie<id>.txt files */
     File moviesDir = new File(dataPath + "/movies");
     FileFilter fileFilter = file -> !file.isDirectory() && file.getName().endsWith(".txt");
     File[] file = moviesDir.listFiles(fileFilter);
     
+    /* Create empty list of movie objects */
     List<Movie> movies = new ArrayList<Movie>();
     
+    /* Loop through list of Movie<id>.txt files */
     for (int i = 0; i < file.length; i += 1) {
       try {
         FileReader fr = new FileReader(file[i].getPath());
         BufferedReader br = new BufferedReader(fr);
         
+        /* Read file line by line */
         int movieID = Integer.parseInt(br.readLine());
         String movieName = br.readLine();
         Movie.Type movieType = Movie.Type.valueOf(br.readLine());
@@ -49,9 +61,11 @@ public class MoblimaInitializer {
         String[] cast = br.readLine().split(",", 0);
         String director = br.readLine();
       
+        /* Create movie object and store into movies list. */
         Movie newMovie = new Movie(movieID, movieName, movieType, movieStatus, movieRating, synopsis, cast, director);
         movies.add(newMovie);
         
+        /* Set ticket sales for PREVIEW/ENDOFSHOWING/NOWSHOWING movies */
         if (movieStatus != Movie.Status.COMINGSOON) {
           int ticketSales = Integer.parseInt(br.readLine());
           newMovie.setTicketSales(ticketSales);
@@ -68,20 +82,29 @@ public class MoblimaInitializer {
   }
   
   
+  /*
+   * Initialise reviews
+   * Returns: List of review objects
+   */
   public List<Review> initializeReview(MovieManager mm) {
+    /* Get list of Review<id>.txt files */
     File reviewsDir = new File(dataPath + "/reviews");
     FileFilter fileFilter = file -> !file.isDirectory() && file.getName().endsWith(".txt");
     File[] file = reviewsDir.listFiles(fileFilter);
     
+    /* Create empty list of review objects */
     List<Review> reviews = new ArrayList<Review>();
     
+    /* Loop through list of Review<id>.txt files */
     for (int i = 0; i < file.length; i += 1) {
       try {
         FileReader fr = new FileReader(file[i].getPath());
         BufferedReader br = new BufferedReader(fr);
         
-        int movieID = Integer.parseInt(br.readLine());
-                
+        /* Read file line by line */
+        
+        /* Get movie object from movie ID */
+        int movieID = Integer.parseInt(br.readLine());   
         Movie m = mm.searchMovie(movieID);
                 
         String customerName = br.readLine();
@@ -89,11 +112,12 @@ public class MoblimaInitializer {
         String reviewBody = br.readLine();
         int reviewRating = Integer.parseInt(br.readLine());
       
+        /* Create review object and update review information on movie object */
         Review newReview = new Review(movieID, customerName, reviewTitle, reviewBody, reviewRating);
-        
         m.addReview(newReview);
         m.updateOverallRating();
         
+        /* Store review object into reviews list */
         reviews.add(newReview);
         
         br.close();
@@ -107,26 +131,41 @@ public class MoblimaInitializer {
   }
   
   
+  /*
+   * Initialise Cineplexes
+   * Parameters: List of movie objects 
+   * Returns: List of cineplex objects
+   */
   public List<Cineplex> initializeCineplex(List<Movie> movies) {
-    File cinemaDir = new File(dataPath + "/cinemas");
-    
+    /* Get list of <CineplexID>.txt files */
     File cineplexDir = new File(dataPath + "/cineplexes");
     FileFilter fileFilter = file -> !file.isDirectory() && file.getName().endsWith(".txt");
     File[] file = cineplexDir.listFiles(fileFilter);
     
+    /* Get the directory of <CinemaID>.txt files */ 
+    File cinemaDir = new File(dataPath + "/cinemas");
+    
+    /* Create empty list of cineplex objects */
     List<Cineplex> cineplexes = new ArrayList<Cineplex>();
     
-    for (int i = 0; i < file.length; i += 1) {
+    /* Loop through list of <CineplexID>.txt files */
+    for (int i = 0; i < file.length; i += 1) { // E.g. SWA
       try {
         FileReader fr = new FileReader(file[i].getPath());
         BufferedReader br = new BufferedReader(fr);
-            
-        String cineplexID = br.readLine();
-        String[] cinemaStr = br.readLine().split(",", 0);
         
+        /* Read file line by line */
+        String cineplexID = br.readLine();
+        
+        /* Get list of cinemas under current cineplex */
+        String[] cinemaStr = br.readLine().split(",", 0); // E.g. SWAA, SWAB, SWAC
+                
+        /* Create empty array of Cinema objects */
         Cinema[] cinemas = new Cinema[cinemaStr.length];
-              
-        for (int j = 0; j < cinemaStr.length; j += 1) {
+        
+        /* Loop through list of <CinemaID>.txt files */
+        for (int j = 0; j < cinemaStr.length; j += 1) { // E.g. SWAA, SWAB, SWAC
+          /* Get file object of current <CinemaID>.txt file */
           String cinemaStrName = cinemaStr[j]; 
           FileFilter ff = filec -> !filec.isDirectory() && filec.getName().endsWith(".txt") && filec.getName().startsWith(cinemaStrName);
           File[] filec = cinemaDir.listFiles(ff);
@@ -134,15 +173,30 @@ public class MoblimaInitializer {
           FileReader frc = new FileReader(filec[0].getPath());
           BufferedReader brc = new BufferedReader(frc);
           
+          /* Read file line by line */
           boolean isPlatinum = Boolean.parseBoolean(brc.readLine());
           String cinemaCode = brc.readLine();
           String cinemaName = brc.readLine();
+                    
+          /* Create empty list of ScreeningTimes objects */
+          List<ScreeningTimes> st = new ArrayList<ScreeningTimes>();
+          
+          /* Initialise 12 ScreeningTimes */
+          String[] times = new String[] {"0900", "1100", "1300", "1500", "1700", "1900", "2100", "2300", "0900", "1200", "1500", "1800"};
+          
+          /* 
+           * Generate array of seat objects
+           * Platinum Cinema:
+           * 50% Elite Seats
+           * 50% Ultima Seats
+           * 
+           * Normal Cinema:
+           * 10 Couple Seats
+           * Remainder Normal Seats
+           */
           
           int noOfSeats = Integer.parseInt(brc.readLine());
           
-          List<ScreeningTimes> st = new ArrayList<ScreeningTimes>();
-          
-          String[] times = new String[] {"0900", "1100", "1300", "1500", "1700", "1900", "2100", "2300", "0900", "1200", "1500", "1800"};
           for (int x = 0; x < 12; x += 1) {
             Seat[] seats = new Seat[noOfSeats];
             
@@ -168,13 +222,16 @@ public class MoblimaInitializer {
               }
             }
             
+            /* Create ScreeningTimes object and store into st list */
             ScreeningTimes s = new ScreeningTimes(cinemaCode, cinemaName, 1, times[x], "13/11/2022", seats);
             st.add(s);
           }
-           
+          
+          /* Create cinema object and store into cinemas array */ 
           cinemas[j] = new Cinema(isPlatinum, cinemaCode, cinemaName, movies, st);
         }
         
+        /* Create cineplex object and store into cineplexes list */
         Cineplex newCineplex = new Cineplex(cineplexID, cinemas);
         cineplexes.add(newCineplex);
         
@@ -189,18 +246,26 @@ public class MoblimaInitializer {
   }
   
   
+  /*
+   * Initialise Customer data
+   * Returns: ArrayList of customer objects
+   */
   public ArrayList<Customer> initializeCustomers() {
+    /* Get list of Customer<id>.txt files */
     File custDir = new File(dataPath + "/users/customers");
     FileFilter fileFilter = file -> !file.isDirectory() && file.getName().endsWith(".txt");
     File[] file = custDir.listFiles(fileFilter);
     
+    /* Create empty ArrayList of Customer objects */
     ArrayList<Customer> customers = new ArrayList<Customer>();
     
+    /* Loop through list of Customer<id>.txt files */
     for (int i = 0; i < file.length; i += 1) {
       try {
         FileReader fr = new FileReader(file[i].getPath());
         BufferedReader br = new BufferedReader(fr);
         
+        /* Read file line by line */
         String email = br.readLine();
         String password = br.readLine();
         int type = Integer.parseInt(br.readLine());
@@ -208,7 +273,8 @@ public class MoblimaInitializer {
         String name = br.readLine();
         int age = Integer.parseInt(br.readLine());
         int id = Integer.parseInt(br.readLine());
-      
+        
+        /* Create customer object and store into customers list. */
         Customer newCustomer = new Customer(email, password, type, mobile_number, name, age, id);
         customers.add(newCustomer);
         
@@ -223,24 +289,33 @@ public class MoblimaInitializer {
   }
   
   
+  /*
+   * Initialise Admin data
+   * Returns: ArrayList of admin objects
+   */
   public ArrayList<Admin> initializeAdmin() {
+    /* Get list of Admin<id>.txt files */
     File adminDir = new File(dataPath + "/users/admin");
     FileFilter fileFilter = file -> !file.isDirectory() && file.getName().endsWith(".txt");
     File[] file = adminDir.listFiles(fileFilter);
     
+    /* Create empty ArrayList of Admin objects */
     ArrayList<Admin> admins = new ArrayList<Admin>();
     
+    /* Loop through list of Admin<id>.txt files */
     for (int i = 0; i < file.length; i += 1) {
       try {
         FileReader fr = new FileReader(file[i].getPath());
         BufferedReader br = new BufferedReader(fr);
         
+        /* Read file line by line */
         String email = br.readLine();
         String password = br.readLine();
         int type = Integer.parseInt(br.readLine());
         int id = Integer.parseInt(br.readLine());
         String cineplexID = br.readLine();
       
+        /* Create admin object and store into customers list */
         Admin newAdmin = new Admin(email, password, type, id, cineplexID);
         admins.add(newAdmin);
         
@@ -255,23 +330,35 @@ public class MoblimaInitializer {
   }
   
   
+  /* 
+   * Initialise Tickets
+   * Parameters: ArrayList of customers, Cineplex object
+   * Returns: TicketManager
+   */
   public TicketManager initializeTickets(ArrayList<Customer> customer, Cineplex cineplex) {
     /* 
-     * 
+     * Ticket Data that we want to intiialise:
+     * -----------------------------------------
      * Cineplex: SWB 
      * Cinema: Shaw Theatres Waterway Point Hall 1
      * Movie: Jurassic World (Movie id: 7)
      * Seat Id: 1, 2, 3
      * Customer: Terence Tang (id: 4) (Child / 12), Rachel Tan (id: 6) (Senior Citizen / 65), Wesley Chan (id: 5) (Adult / 23)
-     * 
      */
     
+    /* Create TicketManager object */
     TicketManager tm = new TicketManager();
     
-    Cinema c = cineplex.getCinemas()[0];   
+    /* Get cinema object */
+    Cinema c = cineplex.getCinemas()[0];
+    
+    /* Get movie object */
     Movie m = c.getMovies().get(7);
+    
+    /* Get ScreeningTime object */
     ScreeningTimes st = c.getScreeningTimes().get(3);
-                    
+    
+    /* Create ticket and transaction for customers */
     for (int i = 0; i < 3; i += 1) {
       int s = st.getSeats()[i].getSeatID();
       tm.createTicket(customer.get(i), s, m, st);
@@ -282,9 +369,14 @@ public class MoblimaInitializer {
   }
   
   
+  /* 
+   * Initialise Seats
+   * Parameters: ArrayList of customers, Cineplex object, TicketManager
+   */
   public void initializeSeats(ArrayList<Customer> customer, Cineplex cineplex, TicketManager tm) {  
     /* 
-     * 
+     * Seat Data that we want to intiialise:
+     * -----------------------------------------
      * Cineplex: SWB 
      * Cinema: Shaw Theatres Waterway Point Hall 1
      * Movie: Jurassic World (Movie id: 7)
@@ -292,12 +384,15 @@ public class MoblimaInitializer {
      * Time: 1500
      * Seat id: 1, 2, 3
      * Ticket id: 1, 2, 3
-     *  
      */
     
+    /* Get cinema object */
     Cinema c = cineplex.getCinemas()[0];
+    
+    /* Get ScreeningTime object */
     ScreeningTimes st = c.getScreeningTimes().get(3);
-            
+    
+    /* Set seat to occupied and set ticketholder to customerid */ 
     for (int i = 0; i < 3 ; i += 1) {
       int s = st.getSeats()[i].getSeatID();
       int t = tm.getTicketid(c.getMovies().get(7).getMovieName(), customer.get(i).getId());
