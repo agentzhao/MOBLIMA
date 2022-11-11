f8d35d3731b7037633738afffaa231d1e3022491
agentzhao <20024592+agentzhao@users.noreply.github.com>
2022-11-11 15:12:42 +0800

    sc.next()

--- src/boundaries/AdminSystem.java
+++ src/boundaries/AdminSystem.java
@@ -6,106 +6,105 @@ import controllers.CineplexManager;
 import controllers.MovieManager;
 import controllers.TicketManager;
 
-
 public class AdminSystem {
-    private MovieManager mm;
-    private CineplexManager cm;
-    private Admin a;
-    private TicketManager tm;
-    public AdminSystem(MovieManager m, CineplexManager c, TicketManager t) {
-        mm = m;
-        cm = c;
-        tm = t;
-    }
-    public void setAdmin(Admin a)
-    {
-        this.a = a;
+  private MovieManager mm;
+  private CineplexManager cm;
+  private Admin a;
+  private TicketManager tm;
+
+  public AdminSystem(MovieManager m, CineplexManager c, TicketManager t) {
+    mm = m;
+    cm = c;
+    tm = t;
+  }
+
+  public void setAdmin(Admin a) {
+    this.a = a;
+  }
+
+  public void viewScreen() {
+    System.out.println("5: Admin Panel\n6: Shutdown MOBLIMA");
+  }
+
+  public void adminPanel() {
+    Scanner sc = new Scanner(System.in);
+    int id;
+    String cid;
+    int choice = 0;
+    while (choice != 8) {
+      System.out.println(
+          "1: Create Movie Listing\n2: Update Movie Listing\n3: Remove Movie Listing\n4:Create Cinema showtime\n5: Update Cinema Showtime\n6: Remove Cinema Showtime\n7: Configure System settings\n8: Update ticket\n9: Exit");
+      choice = sc.nextInt();
+      switch (choice) {
+        case 1:
+          mm.createMovie(mm.getMovieLength() + 1);
+          break;
+        case 2:
+          System.out.println("Enter the movie ID of the movie you want to update");
+          id = sc.nextInt();
+          mm.updateMovie(id);
+          break;
+        case 3:
+          System.out.println("Enter the ID of the movie you want to remove");
+          id = sc.nextInt();
+          mm.deleteMovie(id);
+          break;
+        case 4:
+          System.out.println("Enter the movie ID of the movie you want to add screentime for");
+          id = sc.nextInt();
+          /*
+           * System.out.println("Select the cinema you want to add screentime for");
+           * cid = sc.next();
+           */
+          // cm.createShowtime(a.getcineplexID(), cid, id);
+          cm.createShowtime(a.getcineplexID(), mm.searchMovie(id));
+          break;
+        case 5:
+          System.out.println("Enter the movie ID of the movie you want to update screentime for");
+          id = sc.nextInt();
+          // System.out.println("Select the cinema you want to update screentime for");
+          // cid = sc.next();
+          // cm.updateShowtime(a.getcineplexID(), cid, id);
+          cm.updateShowtime(a.getcineplexID(), mm.searchMovie(id));
+          break;
+        case 6:
+          System.out.println("Enter the movie ID of the movie you want to remove screentime for");
+          id = sc.nextInt();
+          // System.out.println("Select the cinema you want to remove screentime for");
+          // cid = sc.next();
+          // cm.removeShowTime(a.getcineplexID(), cid, id);
+          cm.removeShowTime(a.getcineplexID());
+          break;
+        case 7:
+          System.out.println("1: Set base price\n2: Set modifier");
+          int c = sc.nextInt();
+          switch (c) {
+            case 1:
+              System.out.println("What do you want to update the base price to?");
+              System.out.println("Current base price: " + tm.getBasePrice());
+              tm.setBasePrice(sc.nextInt());
+              break;
+            case 2:
+              tm.updatePrices();
+              break;
+            default:
+              System.out.println("Invalid choice");
+          }
+
+          break;
+        case 8:
+          System.out.println("End the userid that you want to edit");
+          id = sc.nextInt();
+          System.out.println("Enter the movie of the ticket");
+          cid = sc.nextLine();
+          tm.updateTicket(cid, id);
+          break;
+        case 9:
+          break;
+        default:
+          System.out.println("Please input a valid number");
+      }
     }
 
-    public void viewScreen() {
-        System.out.println("5: Admin Panel\n6: Shutdown MOBLIMA");
-    }
-
-    public void adminPanel()
-    {
-        Scanner sc = new Scanner(System.in);
-        int id;
-        String cid;
-        int choice = 0;
-        while(choice != 8)
-        {
-            System.out.println("1: Create Movie Listing\n2: Update Movie Listing\n3: Remove Movie Listing\n4:Create Cinema showtime\n5: Update Cinema Showtime\n6: Remove Cinema Showtime\n7: Configure System settings\n8: Update ticket\n9: Exit");
-            choice = sc.nextInt();
-            switch(choice)
-            {
-                case 1:
-                    mm.createMovie(mm.getMovieLength() + 1);
-                    break;
-                case 2: 
-                    System.out.println("Enter the movie ID of the movie you want to update");
-                    id = sc.nextInt();
-                    mm.updateMovie(id);
-                    break;
-                case 3:
-                    System.out.println("Enter the ID of the movie you want to remove");
-                    id = sc.nextInt();
-                    mm.deleteMovie(id);
-                    break;
-                case 4:
-                    System.out.println("Enter the movie ID of the movie you want to add screentime for");
-                    id = sc.nextInt();
-                    /*System.out.println("Select the cinema you want to add screentime for");
-                    cid = sc.next();*/
-                    //cm.createShowtime(a.getcineplexID(), cid, id); 
-                    cm.createShowtime(a.getcineplexID(), mm.searchMovie(id));
-                    break;
-                case 5:
-                    System.out.println("Enter the movie ID of the movie you want to update screentime for");
-                    id = sc.nextInt();
-                    // System.out.println("Select the cinema you want to update screentime for");
-                    // cid = sc.next();
-                    // cm.updateShowtime(a.getcineplexID(), cid, id); 
-                    cm.updateShowtime(a.getcineplexID(), mm.searchMovie(id));
-                    break;
-                case 6:
-                    System.out.println("Enter the movie ID of the movie you want to remove screentime for");
-                    id = sc.nextInt();
-                    // System.out.println("Select the cinema you want to remove screentime for");
-                    // cid = sc.next();
-                    // cm.removeShowTime(a.getcineplexID(), cid, id);
-                    cm.removeShowTime(a.getcineplexID());
-                    break;
-                case 7:
-                    System.out.println("1: Set base price\n2: Set modifier");
-                    int c = sc.nextInt();
-                    switch(c)
-                    {
-                        case 1:
-                            System.out.println("What do you want to update the base price to?");
-                            System.out.println("Current base price: " + tm.getBasePrice());
-                            tm.setBasePrice(sc.nextInt());
-                            break;
-                        case 2:
-                            tm.updatePrices();
-                            break;
-                        default:
-                            System.out.println("Invalid choice");
-                    }
-                    
-                    break;
-                case 8:
-                    System.out.println("End the userid that you want to edit");
-                    id = sc.nextInt();
-                    System.out.println("Enter the movie of the ticket");
-                    cid = sc.next();
-                    tm.updateTicket(cid, id);
-                    break;
-                case 9:
-                    break;
-                default:
-                    System.out.println("Please input a valid number");
-            }
-        }
-       
-    }
+  }
 }
--- src/boundaries/MOBLIMA.java
+++ src/boundaries/MOBLIMA.java
@@ -19,20 +19,21 @@ public class MOBLIMA {
   private static Admin a;
   private static Customer c;
   private static User tempUser;
-  
+
   public static void main(String[] args) {
-    //ReviewManager rm = new ReviewManager();
-    //SeatManager sm = new SeatManager();
-    //TicketManager tm = new TicketManager();
-    /*Login log = new Login();
-    log.createAdmin("admin@gmail.com", "123", 1, 1234, "1333");
-    log.createCustomer("customer@gmail.com", "123", 0, "123999", "James", 21);*/
-    //System.out.println("Welcome to MOBLIMA");
+    // ReviewManager rm = new ReviewManager();
+    // SeatManager sm = new SeatManager();
+    // TicketManager tm = new TicketManager();
+    /*
+     * Login log = new Login();
+     * log.createAdmin("admin@gmail.com", "123", 1, 1234, "1333");
+     * log.createCustomer("customer@gmail.com", "123", 0, "123999", "James", 21);
+     */
+    // System.out.println("Welcome to MOBLIMA");
     initData();
     mainMenu(log);
     // todo initialize data
-    
-    
+
     /*
      * while (choice < 3) {
      * System.out.
@@ -51,49 +52,47 @@ public class MOBLIMA {
      * }
      */
     System.out.println("Thank you for using MOBLIMA");
-    
+
   }
 
-  public static void initData()
-  {
+  public static void initData() {
     /* Create MoblimaInitializer Object */
     MoblimaInitializer mi = new MoblimaInitializer();
-    
+
     /* Initialise Movies and Reviews and store into MovieManager */
     mm = new MovieManager();
     mm.addMovieList(mi.initializeMovie());
     mm.addReviewList(mi.initializeReview(mm));
-    
+
     /* Initialise Customers and Administrators and store into Login */
     log = new Login();
-    
+
     ArrayList<Customer> customer = new ArrayList<Customer>();
     customer = mi.initializeCustomers();
     log.addCustomerList(customer);
-    
+
     log.addAdminList(mi.initializeAdmin());
-    
+
     /* Initialise Cineplexes and store into CineplexManager */
     cm = new CineplexManager();
     List<Cineplex> cineplexes = mi.initializeCineplex(mm.getMovieList());
     cm.setCineplexes(cineplexes);
-        
+
     /* Initialise 3 Tickets Child, Adult and Senior Citizen */
     tm = new TicketManager();
-    
+
     /* Create ticket and transaction */
     tm = mi.initializeTickets(customer, cm.getCineplexList().get(0));
-            
+
     /* Set seat to occupied based on purchased ticket */
     mi.initializeSeats(customer, cm.getCineplexList().get(0), tm);
-    
+
     us = new UserSystem(mm);
     as = new AdminSystem(mm, cm, tm);
   }
 
-  public static void mainMenu(Login log)
-  {
-   
+  public static void mainMenu(Login log) {
+
     int login = 0, admin = 0;
     Scanner sc = new Scanner(System.in);
     int choice = -1;
@@ -108,7 +107,6 @@ public class MOBLIMA {
       }
       choice = sc.nextInt();
 
-      
       switch (choice) {
         case 1:
           movieMenu();
@@ -116,9 +114,8 @@ public class MOBLIMA {
         case 2:
           System.out.println("Sort by:\n1: Rating\n2: Ticket Sales");
           int t = sc.nextInt();
-          switch(t)
-          {
-            case 1: 
+          switch (t) {
+            case 1:
               mm.topRating();
               break;
             case 2:
@@ -134,7 +131,7 @@ public class MOBLIMA {
           String p = "";
           if (login == 0) {
             // Login
-            //Admin test = (Admin) log.loginSystem("admin@gmail.com", "123");
+            // Admin test = (Admin) log.loginSystem("admin@gmail.com", "123");
             // System.out.println(test.getCinemaId());
             // Customer testc = (Customer) log.loginSystem("customer@gmail.com", 123);
             // System.out.println(testc.getMobile_number());
@@ -154,17 +151,14 @@ public class MOBLIMA {
                 login = 1;
               }
             }
-          }
-          else
-          {
+          } else {
             ArrayList<Ticket> tempTicket = tm.searchTicketUser(c.getId());
-            for(Ticket i : tempTicket)
-            {
+            for (Ticket i : tempTicket) {
               tm.getTicketDetails(i);
               System.out.println("-------------------------------------");
             }
           }
-          
+
           break;
         case 4:
           System.out.println("Thank you for using MOBLIMA");
@@ -175,14 +169,12 @@ public class MOBLIMA {
           as.setAdmin(null);
           break;
         case 5:
-          if(admin == 1)
-          {
+          if (admin == 1) {
             as.adminPanel();
           }
           break;
         case 6:
-        if(admin == 1)
-          {
+          if (admin == 1) {
             System.out.println("Shutting down MOBLIMA");
             sc.close();
           }
@@ -192,32 +184,27 @@ public class MOBLIMA {
           break;
       }
     }
-    
+
   }
-  public static void movieMenu()
-  {
+
+  public static void movieMenu() {
     Scanner sc = new Scanner(System.in);
     System.out.println("Please Enter the movie name: ");
-    String searchName = sc.next();
+    String searchName = sc.nextLine();
     Movie temp = mm.searchMovieName(searchName);
     int choice = 0;
-    if(temp != null)
-    {
+    if (temp != null) {
       mm.getMovieDetails(temp);
-    }
-    else
-    {
+    } else {
       System.out.println("Movie not found");
       return;
     }
-    while(choice != 2)
-    {
+    while (choice != 2) {
       System.out.println("1: Check screen time\n2: Exit");
       choice = sc.nextInt();
-      switch(choice)
-      {
+      switch (choice) {
         case 1:
-          //get screen time for show
+          // get screen time for show
           screenTimeMenu(temp);
           break;
         case 2:
@@ -229,95 +216,78 @@ public class MOBLIMA {
     }
   }
 
-  public static void screenTimeMenu(Movie movie)
-  {
+  public static void screenTimeMenu(Movie movie) {
     ScreeningTimes st;
     int c = 0;
-    while(c != 2)
-    {
+    while (c != 2) {
       st = cm.displayScreentime(null, movie);
       System.out.println("1: Get Seat\n2: Exit");
       Scanner sc = new Scanner(System.in);
       c = sc.nextInt();
-      switch(c)
-      {
+      switch (c) {
         case 1:
-          seatMenu(movie,st);
+          seatMenu(movie, st);
           break;
-        case 2: 
+        case 2:
           break;
         default:
           System.out.println("Please input a valid number");
           break;
       }
     }
-    
-    //sc.close();
+
+    // sc.close();
   }
 
-  public static void seatMenu(Movie movie, ScreeningTimes st)
-  {
-    //Print out Seating plan
-    
+  public static void seatMenu(Movie movie, ScreeningTimes st) {
+    // Print out Seating plan
+
     int c = 0;
     int s = -1;
-    while(c != 2)
-    {
+    while (c != 2) {
       cm.printSeats(st);
-      if(tempUser != null)
-      {
+      if (tempUser != null) {
         System.out.println("1: Book ticket\n2: Exit");
-      }
-      else
-      {
+      } else {
         System.out.println("1: Exit");
       }
       Scanner sc = new Scanner(System.in);
       c = sc.nextInt();
-      switch(c)
-      {
+      switch (c) {
         case 1:
-          if(tempUser != null)
-          {
+          if (tempUser != null) {
             System.out.println("Please select a seat");
             s = sc.nextInt();
             bookingMenu(movie, st, s);
-          }
-          else
-          {
+          } else {
             System.out.println("Exiting Seat Menu");
             return;
           }
           break;
         case 2:
-          if(tempUser == null)
-          {
+          if (tempUser == null) {
             System.out.println("Please input a valid number");
           }
           break;
         default:
           System.out.println("Please input a valid number");
           break;
-          
+
       }
     }
-    
-    //sc.close();
+
+    // sc.close();
   }
 
-  public static void bookingMenu(Movie movie, ScreeningTimes st , int s)
-  {
-    if(tempUser.getType() == 2)
-    {
-      //Waiting for ticketmanager update
-      //tm.createTicket(c, Movie class);
-      Ticket tempTicket = tm.createTicket(c ,s , movie, st);
-      cm.bookSeat(st, s, tempTicket.getTicketID()); //placeholder
+  public static void bookingMenu(Movie movie, ScreeningTimes st, int s) {
+    if (tempUser.getType() == 2) {
+      // Waiting for ticketmanager update
+      // tm.createTicket(c, Movie class);
+      Ticket tempTicket = tm.createTicket(c, s, movie, st);
+      cm.bookSeat(st, s, tempTicket.getTicketID()); // placeholder
       System.out.println("Book ticket");
-    }
-    else
-    {
-      //tm.createTicket(c, Movie class);
+    } else {
+      // tm.createTicket(c, Movie class);
       System.out.println("Admin cannot book ticket");
     }
   }
