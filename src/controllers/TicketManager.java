c2fe384c8c8e80f1dd9340d49629dd43f62307e6
Keith Sim <keithsimks@gmail.com>
2022-11-12 16:06:22 +0800

    update

--- src/controllers/TicketManager.java
+++ src/controllers/TicketManager.java
@@ -34,10 +34,10 @@ public class TicketManager{
     Scanner sc = new Scanner(System.in);
 
     //constructor
-    public TicketManager() {
+    public TicketManager(ArrayList<String> holidayDates) {
         ticket = new ArrayList<Ticket>();
         transactions = new ArrayList<Transaction>();
-        holidayDates= new ArrayList<String>();
+        this.holidayDates= holidayDates;
 
         this.basePrice=12;
 
